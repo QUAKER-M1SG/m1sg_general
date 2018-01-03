f49ded6026e37012bfb08b9a61543b0a833bc477
MTEST <m1sgmail@163.com>
2018-01-03 14:34:50 +0800

    add general_date_time function

--- m1sg_general.js
+++ m1sg_general.js
@@ -60,5 +60,41 @@ $.extend({
 			console.log(result);
 		}
 		return result;
+	},
+	/**
+	 * [general_date_time 日期时间格式化函数]
+	 * @param  {[string]} type [需要转换的格式化名称]
+	 * @return {[string]}      [转换后的格式化日期时间]
+	 */
+	general_date_time:function(type){
+		var result;
+		Date.prototype.Format = function (fmt){ 
+		    var o = {  
+		        "M+":this.getMonth() + 1, //月份   
+		        "d+":this.getDate(), //日   
+		        "H+":this.getHours(), //小时   
+		        "m+":this.getMinutes(), //分   
+		        "s+":this.getSeconds(), //秒   
+		        "q+":Math.floor((this.getMonth() + 3) / 3), //季度   
+		        "S":this.getMilliseconds() //毫秒   
+		    };  
+		    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));  
+		    for (var k in o)  
+		    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));  
+		    return fmt;  
+		};  
+		var nowTime = new Date();  
+		switch(type){
+			case 'date':
+				result = nowTime.Format("yyyy.MM.dd");
+			break;
+			case 'date-time':
+				result = nowTime.Format("yyyy-MM-dd HH:mm:ss");
+			break;
+			case 'time':
+				result = nowTime.Format("mm分ss秒")
+			break;
+		};
+		return result;
 	}
 });
