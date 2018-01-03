1c0e4dd82950f06f9bb9bf5506dc7b8df31a369e
QUAKER-M1SG <m1sgmail@163.com>
2018-01-03 14:20:01 +0800

    Update m1sg_general.js

--- m1sg_general.js
+++ m1sg_general.js
@@ -1,17 +1,26 @@
 $.extend({
 	/**
-	 * [general_divide_half 根据类型返回传入的一组元素中前半或后半部分元素]
+	 * [general_divide_half 根据类型返回传入的一组元素中前半或后半或奇数位置偶数位置元素]
 	 * @param  {[object]} element [传入的一组封装为jquery对象的dom元素]
-	 * @param  {[string]} type    ['da'或'xiao']
+	 * @param  {[string]} type    ['da','xiao','ji','ou']
 	 * @return {[object]}         [返回的前半或后半部分封装为jquery对象的dom元素]
 	 */
 	general_divide_half:function(element,type){
 		var half = element.length / 2;
 		var element_select;
-		if(type == 'da'){
-			element_select = element.slice(half,element.length);
-		}else if(type == 'xiao'){
-			element_select = element.slice(0,half);
+		switch(type){
+			case 'da':
+				element_select = element.slice(half,element.length);
+			break;
+			case 'xiao':
+				element_select = element.slice(0,half);
+			break;
+			case 'ji':
+				element_select = element.filter(':even');			 
+			break;
+			case 'ou':
+				element_select = element.filter(':odd');
+			break;
 		}
 		return element_select;
 	},
